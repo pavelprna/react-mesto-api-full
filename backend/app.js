85fd0001154c0609e41fd30707a5b959bb642a9c
Pavel Zharov <pavel@prna.ru>
2021-10-02 00:11:39 +0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://mesto.prna.nomoredomains.club',
-}));
+app.use(cors());
 
 app.use(cookieParser());
 
