e929ed5d8043b224b5faa0fd6ffd0d3751fb7445
Pavel Zharov <pavel@prna.ru>
2021-10-03 01:37:03 +0700

    cookies

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -22,6 +22,8 @@ const login = (req, res, next) => {
       return res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
+        sameSite: 'None',
+        secure: true,
       })
         .send({ message: 'куки установлены' });
     })
