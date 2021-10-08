395642f889f29b383cb2f2f91b74c21d2399f2b0
Pavel Zharov <pavel@prna.ru>
2021-10-09 01:54:00 +0700

    logger

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
 const { userValidator } = require('./middlewares/validation');
-// const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
 
@@ -41,14 +42,13 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(requestLogger);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   // useCreateIndex: true,
   // useFindAndModify: false
 });
-
-// app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -64,12 +64,11 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Ресурс не найден' });
+app.use('*', () => {
+  throw new NotFoundError({ message: 'Ресурс не найден' });
 });
 
-// app.use(errorLogger);
-
+app.use(errorLogger);
 app.use(errors());
 app.use(error);
 
