29dc87ff412d83fbf1ff69bc62196d8263a37233
Pavel Zharov <pavel@prna.ru>
2021-10-09 00:02:54 +0700

    fix: logger

--- backend/app.js
+++ backend/app.js
@@ -47,13 +47,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   // useFindAndModify: false
 });
 
+app.use(requestLogger);
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
-app.use(requestLogger);
 
 app.post('/signin', userValidator, login);
 app.post('/signup', userValidator, createUser);
