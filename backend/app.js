59cf302d068d349b6207eb7eebf4d5b958377e52
Pavel Zharov <pavel@prna.ru>
2021-10-09 10:11:03 +0700

    cors

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-
 const allowList = [
   'https://mesto.prna.nomoredomains.club',
   'http://mesto.prna.nomoredomains.club',
@@ -52,7 +51,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   // useFindAndModify: false
 });
 
-app.use(requestLogger);
+// app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
